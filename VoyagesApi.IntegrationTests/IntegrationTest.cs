cf534d3a2e3b523cdaa21d5e6a5bb10e6eda9f20
Robert Cazacu <robert.cazacu@imotions.com>
2022-05-23 22:19:24 +0200

    Add integration tests project.

---
+++ VoyagesApi.IntegrationTests/IntegrationTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using VoyagesApi.Models;
+
+namespace VoyagesApi.IntegrationTests
+{
+    public class IntegrationTest
+    {
+        protected readonly HttpClient TestClient;
+
+        protected IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>();
+            TestClient = appFactory.CreateClient();
+        }
+
+        protected async Task AuthenticateAsync()
+        {
+            TestClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", await GetJwtAsync());
+        }
+
+        private async Task<string> GetJwtAsync()
+        {
+            var response = await TestClient.PostAsJsonAsync("/api/Login", new UserLogin 
+            {
+                Username = "admin",
+                Password = "admin_PW"
+            });
+
+            var registrationResponse = await response.Content.ReadAsStringAsync();
+            return registrationResponse;
+        }
+    }
+}
---
+++ VoyagesApi.IntegrationTests/VoyagesControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using VoyagesApi.Models;
+using Xunit;
+
+namespace VoyagesApi.IntegrationTests
+{
+    public class VoyagesControllerTests : IntegrationTest
+    {
+        [Fact]
+        public async Task GetAll_Returns_Ok_Response()
+        {
+            // Arrange
+            await AuthenticateAsync();
+
+            // Act
+            var response = await TestClient.GetAsync("/api/Voyages/GetAll");
+
+            //// Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetAverage_Returns_Ok_Response()
+        {
+            // Arrange
+            await AuthenticateAsync();
+
+            // Act
+            var response = await TestClient.GetAsync($"/api/Voyages/GetAverage(451S,{Currency.EUR})");
+
+            //// Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetAverage_Returns_Unauthorized()
+        {
+            // Arrange
+            //await AuthenticateAsync();
+
+            // Act
+            var response = await TestClient.GetAsync($"/api/Voyages/GetAverage(451S,{Currency.EUR})");
+
+            //// Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+    }
+}
