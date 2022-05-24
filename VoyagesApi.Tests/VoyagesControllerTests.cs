78ef72c69e9d4260f6986ee18993a7b44d419825
Robert Cazacu <robert.cazacu@imotions.com>
2022-05-24 17:12:00 +0200

    Add unit test for GetAverage.

--- VoyagesApi.Tests/VoyagesControllerTests.cs
+++ VoyagesApi.Tests/VoyagesControllerTests.cs
@@ -41,5 +41,40 @@ namespace VoyagesApi.Tests
             var returnedVoyages = result.Value as IEnumerable<Voyage>;
             Assert.Equal(5, returnedVoyages.Count());
         }
+
+        [Fact]
+        public void GetAverage_Returns_Correct_Average()
+        {
+            //Arrange
+            var services = new ServiceCollection();
+            services.AddMemoryCache();
+            var serviceProvider = services.BuildServiceProvider();
+            var memoryCache = serviceProvider.GetService<IMemoryCache>();
+
+            var logger = Mock.Of<ILogger<VoyagesController>>();
+
+            var fakeVoyages = A.CollectionOfDummy<Voyage>(10).AsEnumerable();
+            var dataStore = A.Fake<IVoyageDataStore>();
+            A.CallTo(() => dataStore.GetAll()).Returns(fakeVoyages);
+
+            var controller = new VoyagesController(memoryCache, logger, dataStore);
+
+            //Update values
+            var code = "123d";
+            var price = 5;
+            foreach (var voyage in fakeVoyages)
+            {
+                voyage.VoyageCode = code;
+                voyage.Price = price;
+            }
+           
+            //Act
+            var actionResult = controller.GetAverage(code, Currency.USD);
+
+            //Assert
+            var result = actionResult as OkObjectResult;
+            var returnedAverage = (decimal)result.Value;
+            Assert.Equal(price, returnedAverage);
+        }
     }
 }
